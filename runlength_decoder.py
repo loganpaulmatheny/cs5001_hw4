a417991bf1cf694ad4ab115c8a78bff3dd878e76
Logan Matheny <loganpaulmatheny@gmail.com>
2024-10-03 21:08:46 -0400

    Create decode function

--- runlength_decoder.py
+++ runlength_decoder.py
@@ -15,42 +15,31 @@ def decode(data: list) -> list:
     # count the number of charcaters that are the same
     # Methods
     # Pseduo
-    # Create a value holder, and an occurance holder
-    # Read the character if it's different than holder, change value holder to character and change occurance count to one
-    # Check next character if it's the same add to occurance count
-    # If you get a new character
-    # Add the previous to your decoded list and then
-    # Change occurence value, reset the count to 1
-    # Loop until your done with the length of the data
+    # First character and all the odd characters represent the char to create copies of
+    # Second and even characters are how many times to print it
+    # Look at the first character - save it
+    # Read the second character - loop and add saved value to your new decoded list
+    # Add 2 to the counter and repeat
+    decoded_lst = []
 
-    value_holder = data[0]
-    print(value_holder)
-    occurrence_holder = 0
-
-    decoded_list = []
-
-    for i in data:
-        if data[i] == value_holder:
-            occurrence_holder += 1
-        else:
-            decoded_list.append(value_holder)
-            decoded_list.append(occurrence_holder)
-
-            value_holder = data[i]
-            occurrence_holder = 1
-
-    return decoded_list
-    print(decoded_list)
+    for i in range(0, len(data), 2):
+        secret_char = data[i]
+        secret_num = data[i + 1]
+        for j in range(0, secret_num):
+            decoded_lst.append(secret_char)
+    print(decoded_lst)
 
 
 def main():
     decode(hw4data.DATA0)
-   # print(decode(hw4data.DATA0))
-   # print(decode(hw4data.DATA1))
-   # print(decode(hw4data.DATA2))
-   # print(decode(hw4data.DATA3))
-   # print(decode(hw4data.DATA4))
-   # print(decode(hw4data.DATA5))
+
+
+# print(decode(hw4data.DATA0))
+# print(decode(hw4data.DATA1))
+# print(decode(hw4data.DATA2))
+# print(decode(hw4data.DATA3))
+# print(decode(hw4data.DATA4))
+# print(decode(hw4data.DATA5))
 
 
 if __name__ == "__main__":
